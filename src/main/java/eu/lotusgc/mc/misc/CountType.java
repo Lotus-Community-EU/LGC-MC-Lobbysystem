dfa47cff76334798934a523029535a49cb7434aa
Christopher Wilhelm H. <info@kitsoft.at>
2024-01-08 14:03:47 +0100

    Serverdata is more descriptive than CountType. Thus new name and more enums

--- src/main/java/eu/lotusgc/mc/misc/CountType.java
+++
@@ -1,7 +0,0 @@
-package eu.lotusgc.mc.misc;
-
-public enum CountType {
-	CURRENT_PLAYERS,
-	CURRENT_STAFF,
-	MAX_ALL
-}
---
+++ src/main/java/eu/lotusgc/mc/misc/Serverdata.java
@@ -0,0 +1,13 @@
+package eu.lotusgc.mc.misc;
+
+public enum Serverdata {
+	CurrentPlayers,
+	CurrentStaffs,
+	MaxPlayers,
+	OnlineStatus,
+	LockedStatus,
+	MaintenanceStatus,
+	FancyName,
+	IsHybrid,
+	RequiredJoinlevel;
+}
