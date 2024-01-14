c1f1db7997afa46a921a1615f33121ab94a8a6aa
Christopher Wilhelm H. <info@kitsoft.at>
2024-01-15 00:20:44 +0100

    Added the event blocker

---
+++ src/main/java/eu/lotusgc/mc/event/EventBlocker.java
@@ -0,0 +1,73 @@
+package eu.lotusgc.mc.event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import eu.lotusgc.mc.ext.LotusController;
+
+public class EventBlocker implements Listener{
+	
+	@EventHandler
+	public void onDamage(EntityDamageEvent event) {
+		List<DamageCause> causes = new ArrayList<>();
+		causes.add(DamageCause.ENTITY_ATTACK);
+		causes.add(DamageCause.FALL);
+		causes.add(DamageCause.CONTACT);
+		causes.add(DamageCause.LAVA);
+		causes.add(DamageCause.DROWNING);
+		causes.add(DamageCause.FIRE);
+		causes.add(DamageCause.WITHER);
+		causes.add(DamageCause.HOT_FLOOR);
+		causes.add(DamageCause.FREEZE);
+		
+		Entity entity = event.getEntity();
+		if(entity instanceof Player) {
+			if(causes.contains(event.getCause())) {
+				event.setCancelled(true);
+			}
+		}
+	}
+	
+	public void onHunger(FoodLevelChangeEvent event) {
+		event.setCancelled(true);
+	}
+	
+	@EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent e) {
+    	Player p = e.getPlayer();
+    	String message = e.getMessage();
+    	LotusController lc = new LotusController();
+    	if(message.equalsIgnoreCase("/pl") || message.equalsIgnoreCase("/plugins") || message.equalsIgnoreCase("/bukkit:pl") || message.equalsIgnoreCase("/bukkit:plugins")) {
+    		if(p.hasPermission("*")) {
+    			e.setCancelled(false);
+    		}else {
+    			e.setCancelled(true);
+    			lc.noPerm(p, "bukkit.command.plugins");
+    		}
+    	}else if(message.equalsIgnoreCase("/help") || message.equalsIgnoreCase("/?") || message.equalsIgnoreCase("/bukkit:?") || message.equalsIgnoreCase("/bukkit:help") || message.equalsIgnoreCase("/minecraft:help")) {
+    		if(p.hasPermission("*")) {
+    			e.setCancelled(false);
+    		}else {
+    			e.setCancelled(true);
+    			lc.noPerm(p, "bukkit.command.help");
+    		}
+    	}else if(message.equalsIgnoreCase("/icanhasbukkit") || message.equalsIgnoreCase("/bukkit:about") || message.equalsIgnoreCase("/about") || message.equalsIgnoreCase("/bukkit:ver") || message.equalsIgnoreCase("/bukkit:version")) {
+    		if(p.hasPermission("*")) {
+    			e.setCancelled(false);
+    		}else {
+    			e.setCancelled(true);
+    			lc.noPerm(p, "bukkit.command.version");
+    		}
+    	}
+    }
+
+}
--- src/main/java/eu/lotusgc/mc/main/LotusManager.java
+++ src/main/java/eu/lotusgc/mc/main/LotusManager.java
@@ -8,6 +8,7 @@ import org.bukkit.plugin.PluginManager;
 
 import eu.lotusgc.mc.command.BuildCMD;
 import eu.lotusgc.mc.command.SpawnSystem;
+import eu.lotusgc.mc.event.EventBlocker;
 import eu.lotusgc.mc.event.InventorySetterHandling;
 import eu.lotusgc.mc.event.JoinEvent;
 import eu.lotusgc.mc.event.LeaveEvent;
@@ -70,6 +71,7 @@ public class LotusManager {
 		pm.registerEvents(new ScoreboardHandler(), Main.main);
 		pm.registerEvents(new SpawnSystem(), Main.main);
 		pm.registerEvents(new InventorySetterHandling(), Main.main);
+		pm.registerEvents(new EventBlocker(), Main.main);
 		
 		Bukkit.getConsoleSender().sendMessage("§aMain-Initialisation took §6" + (System.currentTimeMillis() - current) + "§ams");
 	}
